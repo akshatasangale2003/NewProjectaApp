261dd64dd5011558624a2075cd3da86f408c7f7e
vinayart <vinayart36@gmail.com>
2025-12-17 11:34:43 +0530

    chnage the main svelte file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Hi my name is Akshata This is for only the push on the repo sample</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
